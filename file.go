8a4c4647abba60d07deb918585991c7576e1b30d
AlasdairF <AlasdairF@users.noreply.github.com>
2015-07-28 23:48:23 +0700

    Added Copy function

--- file.go
+++ file.go
@@ -3,6 +3,8 @@ package file
 import (
  "os"
  "os/exec"
+ "io"
+ "fmt"
 )
 
 func Exists(f string) bool {
@@ -61,4 +63,53 @@ func CountDir(dirname string) (int, error) {
 		return 0, err
 	}
 	return len(lst), nil
+}
+
+func Copy(src, dst string) (err error) {
+    sfi, err := os.Stat(src)
+    if err != nil {
+        return
+    }
+    if !sfi.Mode().IsRegular() {
+        // cannot copy non-regular files (e.g., directories,
+        // symlinks, devices, etc.)
+        return fmt.Errorf("CopyFile: non-regular source file %s (%q)", sfi.Name(), sfi.Mode().String())
+    }
+    dfi, err := os.Stat(dst)
+    if err != nil {
+        if !os.IsNotExist(err) {
+            return
+        }
+    } else {
+        if !(dfi.Mode().IsRegular()) {
+            return fmt.Errorf("CopyFile: non-regular destination file %s (%q)", dfi.Name(), dfi.Mode().String())
+        }
+        if os.SameFile(sfi, dfi) {
+            return
+        }
+    }
+    if err = os.Link(src, dst); err == nil {
+        return
+    }
+	// copyFileContents
+    in, err := os.Open(src)
+    if err != nil {
+        return
+    }
+    defer in.Close()
+    out, err := os.Create(dst)
+    if err != nil {
+        return
+    }
+    defer func() {
+        cerr := out.Close()
+        if err == nil {
+            err = cerr
+        }
+    }()
+    if _, err = io.Copy(out, in); err != nil {
+        return
+    }
+    err = out.Sync()
+    return
 }
